4b5e1c0d577a7226989b2ff7b338ea0ba127eac5
ashfiyakapali <ashfiyakapali@gmail.com>
2025-05-31 13:30:37 +0530

    save

---
+++ StudentCourses.java
@@ -0,0 +1,12 @@
+package p2_308;
+
+    public class StudentCourses {
+    public static void main(String[] args) {
+        System.out.println("Semester 1: Java - 85, DBMS - 78");
+        System.out.println("Semester 2: OS - 90, CN - 88");
+    }
+}
+
+
+
+
